8fc326cedb139d6459f97f9f337d2442f232b4a1
LeslieJR <l.reyes.ubiqum@gmail.com>
2022-02-25 20:10:24 +0100

    product edit

--- components/Product.vue
+++ components/Product.vue
@@ -1,75 +1,38 @@
 <template>
-    <v-card>
-        <v-btn
-        v-if="isAuth"
-        fab
-        small
-        color="red"
-        class="white--text btn-delete"
-        @click="remove(product._id)"
-        >
-        <v-icon>
-            {{ icons.mdiDelete }}
-        </v-icon>
-        </v-btn>
-        <v-btn
-        v-if="isAuth"
-        fab
-        small
-        color="blue"
-        class="white--text btn-edit"
-        @click="editProduct(product)"
-        >
-        <v-icon>
-            {{ icons.mdiSquareEditOutline }}
-        </v-icon>
-        </v-btn>
-        <v-carousel height="200" hide-delimiter-background show-arrows-on-hover>
-        <v-carousel-item
+  <v-card>
+    <v-carousel height="200" hide-delimiter-background show-arrows-on-hover>
+      <v-carousel-item
         v-for="(img, i) in product.images"
         :key="i"
         :src="img"
         reverse-transition="fade-transition"
         transition="fade-transition"
         @click="details(product._id)"
-        ></v-carousel-item>
-        </v-carousel>
-        <v-card-text class="card-text" @click="details(product._id)">
-        {{ product.name }}
-        </v-card-text>
-    </v-card>
+      ></v-carousel-item>
+    </v-carousel>
+    <v-card-text class="card-text" @click="details(product._id)">
+      {{ product.name }}
+    </v-card-text>
+    <v-card-actions>
+      <slot name="button"></slot>
+    </v-card-actions>
+  </v-card>
 </template>
 <script>
-import { deleteProduct } from "../services";
-import { mdiDelete, mdiSquareEditOutline } from "@mdi/js";
 export default {
-    props:{
-        product: Object,
-    },
+  props: {
+    product: Object,
+  },
   data() {
     return {
-      icons: { mdiDelete, mdiSquareEditOutline },
     };
   },
-  //only users who are logged in (have token) can delete products
-  computed: {
-    isAuth() {
-      return this.$store.getters["user/getToken"];
-    },
-  },
+  
   methods: {
-    async remove(productId) {
-      const token = localStorage.getItem("token");
-      const data = await deleteProduct(productId, token);
-      if (data.err) {
-        alert(data.err);
-      } else {
-        alert("product deleted");
-      }
-    },
+    
     details(productId) {
       this.$router.push(`/details/${productId}`);
-    }
+    },
   },
 };
 </script>
@@ -79,15 +42,5 @@ export default {
   padding-bottom: 5px;
   font-size: 1rem;
 }
-.btn-delete {
-  position: absolute;
-  z-index: 999;
-  right: 0;
-}
-.btn-edit {
-  position: absolute;
-  z-index: 999;
-  right: 0;
-  top: 42px;
-}
+
 </style>
--- components/Products.vue
+++ components/Products.vue
@@ -5,30 +5,58 @@
       <v-col
         class="d-flex"
         :cols="cols"
-        v-for="(product,index) in products"
+        v-for="(product, index) in products"
         :key="index"
       >
         <!-- Product component -->
-        <Product :product="product" />
+        <Product :product="product">
+          <template v-slot:button>
+            <v-btn
+              v-if="isAuth"
+              fab
+              small
+              color="red"
+              class="white--text btn-delete"
+              @click.stop="remove(product, index)"
+            >
+              <v-icon>
+                {{ icons.mdiDelete }}
+              </v-icon>
+            </v-btn>
+            <v-btn
+              v-if="isAuth"
+              fab
+              small
+              color="blue"
+              class="white--text btn-edit"
+              @click.stop="editProduct(product, index)"
+            >
+              <v-icon>
+                {{ icons.mdiSquareEditOutline }}
+              </v-icon>
+            </v-btn>
+          </template>
+        </Product>
         <!-- End Product component -->
-
       </v-col>
     </v-row>
-    <Dialog v-model=showDialog :product="element" />
+    <Dialog v-model="showDialog" :product="element" />
   </div>
 </template>
 <script>
-import { getProducts } from "../services";
-
+import { getProducts,deleteProduct } from "../services";
+import { mdiDelete, mdiSquareEditOutline } from "@mdi/js";
 export default {
   data() {
     return {
+      icons: { mdiDelete, mdiSquareEditOutline },
       products: [],
       onFetch: undefined,
       showDialog: false,
-      element:null
+      element: null,
     };
   },
+  
   async beforeMount() {
     await this.loadProducts();
     this.onFetch = setInterval(async () => {
@@ -39,6 +67,9 @@ export default {
     clearInterval(this.onFetch);
   },
   computed: {
+    isAuth() {
+      return this.$store.getters["user/getToken"];
+    },
     cols() {
       switch (this.$vuetify.breakpoint.name) {
         case "xs":
@@ -59,13 +90,20 @@ export default {
         this.products = data;
       } catch (err) {}
     },
-    editProduct(product){
+    editProduct(product) {
       console.log(product);
-      this.showDialog = true
-      this.element = product
+      this.showDialog = true;
+      this.element = product;
     },
-
-    
+    async remove(productId) {
+      const token = localStorage.getItem("token");
+      const data = await deleteProduct(productId, token);
+      if (data.err) {
+        alert(data.err);
+      } else {
+        alert("product deleted");
+      }
+    },
   },
 };
 </script>
@@ -75,5 +113,16 @@ h2 {
   font-size: 2rem;
   font-family: "Kanit", sans-serif;
 }
-
+ .btn-delete {
+  position: absolute;
+  z-index: 999;
+  right: 0;
+  top:0;
+}
+.btn-edit {
+  position: absolute;
+  z-index: 999;
+  right: 0;
+  top: 42px;
+} 
 </style>
